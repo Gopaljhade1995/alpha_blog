edd3dcf3777b3b6ab18319d6b92116304e42e267
gopal <gopaljhade@gmail.com>
2023-03-24 16:06:24 +0530

    complete code deploy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
+  
   end
   def index
      @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -9,7 +12,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
-    @article = Article.find(params[:id])
+    
   end
   def create
     @article = Article.new(article_params)
@@ -22,7 +25,6 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
@@ -32,7 +34,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
@@ -42,6 +43,12 @@ class ArticlesController < ApplicationController
   end
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 	def index
 		@users = User.paginate(page: params[:page], per_page: 5)
 	end
@@ -38,6 +40,8 @@ class UsersController < ApplicationController
 
     def destroy
       @user.destroy
+      session[:user_id] = nil if @user == current_user
+      flash[:notice] = "Account and all associated articles successfully deleted"
       redirect_to articles_path
     end
 
@@ -48,6 +52,12 @@ class UsersController < ApplicationController
     end
     def set_user
       @user = User.find(params[:id])
+    end
+    def require_same_user
+     if current_user != @user && !current_user.admin?
+       flash[:alert] = "You can only edit or delete  your own account"
+       redirect_to @user
+      end
     end
 
 end
