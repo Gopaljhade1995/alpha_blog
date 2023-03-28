017804292c5dfebb781b90668091f17ad113adcc
gopal <gopaljhade@gmail.com>
2023-03-28 18:26:25 +0530

    complete category index and show and edit action apply

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,6 +20,20 @@ class CategoriesController < ApplicationController
   end
   def show
   	  @category = Category.find(params[:id])
+  	  @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
   end
 
   private
