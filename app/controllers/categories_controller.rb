92ad626b234b172c9558cd0badc58cfb7e32b2e9
gopal <gopaljhade@gmail.com>
2023-03-28 11:59:45 +0530

    create and interation in category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,28 @@
 class CategoriesController < ApplicationController
-	def index
-	end
 
-	def new 
-	end
+  def new
+    @category = Category.new
+  end
 
-	def show
-	end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def index
+  end
+  def show
+  	  @category = Category.find(params[:id])
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
